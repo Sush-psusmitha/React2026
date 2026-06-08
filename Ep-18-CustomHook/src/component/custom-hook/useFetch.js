62cf6fea0900444bfc1a27f405fd5ce0e8e5a49a
Sush-psusmitha <sushsusmitha13@gmail.com>
2026-06-08 19:38:37 +0530

    test

---
+++ Ep-18-CustomHook/src/component/custom-hook/useFetch.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+
+function useFetch(url){
+  let [products,setProducts] = useState([]); 
+  let [error, setError] = useState("");
+  let [isLoading,setIsloading] = useState(true);
+
+  useEffect(()=>{
+   let fetchApi = async () => {
+       try{
+        // let response  = await fetch(url);
+        // if(response.ok){
+            //   let data = await response.json(); 
+            //   setProducts(data)
+            // }
+            // else{
+                //   throw new Error("Search Proper Data!!")
+                // }
+                
+       let response = await axios.get(url); 
+       setProducts(response.data)
+        
+       }
+       catch(error){
+           setError(error.message)
+       }
+       finally{
+        setIsloading(false)
+       }
+   }; 
+   fetchApi()
+  },[]); 
+  return {
+    products,error,isLoading,setProducts
+  }
+}
+export default useFetch
